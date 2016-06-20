e41181a88f2ee49ec2678ca373c7ab0a450921d2
Paul Garner <p.j.garner@gmail.com>
2016-06-20 13:26:50 +0100

    ditto

--- src/main/java/uk/me/paulgarner/fh/service/PersonService.java
+++ src/main/java/uk/me/paulgarner/fh/service/PersonService.java
@@ -1,17 +1,18 @@
 package uk.me.paulgarner.fh.service;
 
 import javax.annotation.ManagedBean;
+import javax.ejb.Stateful;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 
 import uk.me.paulgarner.fh.domain.Person;
 
-@ManagedBean
-@Stateless
+@Stateful
 public class PersonService {
 
-	@PersistenceContext
+    @PersistenceContext(unitName = "fh", type = PersistenceContextType.EXTENDED)
     private EntityManager entityManager;
  
 	public Person find(Long id) {

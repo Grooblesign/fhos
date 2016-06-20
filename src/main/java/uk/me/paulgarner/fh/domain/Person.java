373fd44bdfb18f53a90eecad50ccb3f0c828e8eb
Paul Garner <p.j.garner@gmail.com>
2016-06-20 11:28:25 +0100

    db stuff

--- src/main/java/uk/me/paulgarner/fh/domain/Person.java
+++ src/main/java/uk/me/paulgarner/fh/domain/Person.java
@@ -1,8 +1,19 @@
 package uk.me.paulgarner.fh.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Person {
 
+	@Id
+	@Column
+	private long id;
+	
+	@Column
+	private String forenames;
+
+	@Column
+	private String surname;
 }

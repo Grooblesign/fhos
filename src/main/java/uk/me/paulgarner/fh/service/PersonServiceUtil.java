f333debea61ee4a4fc6d740d9d7dead5ca94de5f
Paul Garner <p.j.garner@gmail.com>
2016-06-20 16:15:18 +0100

    ditto

--- src/main/java/uk/me/paulgarner/fh/service/PersonServiceUtil.java
+++ src/main/java/uk/me/paulgarner/fh/service/PersonServiceUtil.java
@@ -12,7 +12,21 @@ public class PersonServiceUtil {
 	@Inject
     private PersonService personService;
 	
+	private String test;
+	
 	public Person findById(long id) {
 		return personService.find(id);
+	}
+
+	public String getTest() {
+		// findById(55287).getSurname()
+		
+		test = "Wibble";
+		
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
 	}
 }

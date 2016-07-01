fd6ab0dbfb8d023b6b3669eff8572fd23c934582
Paul Garner <p.j.garner@gmail.com>
2016-07-01 18:29:02 +0100

    Update

--- src/main/java/uk/me/paulgarner/fh/util/DateValueCalculator.java
+++ src/main/java/uk/me/paulgarner/fh/util/DateValueCalculator.java
@@ -12,61 +12,63 @@ public class DateValueCalculator {
 		
 		String months = "JANFEBMARAPRMAYJUNJULAUGSEPOCTNOVDEC";
 
-		int dashPos = dateString.indexOf("-");
-		if (dashPos > -1) {
-			dateString = dateString.substring(0, dashPos-1);
-		}
-		
-		if (dateString.substring(0, 3).equalsIgnoreCase("bef")) {
-			dateString = dateString.substring(4);
-			adjustment = -1;
-		}
-
-		if (dateString.substring(0, 3).equalsIgnoreCase("aft")) {
-			dateString = dateString.substring(4);
-			adjustment = 1;
-		}
-
-		Pattern pattern = Pattern.compile("^[0-9]{4}");
-		Matcher matcher = pattern.matcher(dateString);
-		if (matcher.find()) {
-			result = Integer.parseInt(dateString.substring(0,4));
-		}
-
-		pattern = Pattern.compile("^Q[0-9]{1} ([0-9]{4})$");
-		matcher = pattern.matcher(dateString);
-		if (matcher.find()) {
-			Integer value = Integer.parseInt(dateString.substring(3,7));
-			result = (value * 10000) + 100;
+		if ((null != dateString) && (dateString.length() > 0)) {
+			int dashPos = dateString.indexOf("-");
+			if (dashPos > -1) {
+				dateString = dateString.substring(0, dashPos-1);
+			}
 			
-			value = Integer.parseInt(dateString.substring(1, 2));
-			result = result + ((value - 1) * 300);
-		}
-		
-		pattern = Pattern.compile("^[0-9]{2} [a-zA-Z]{3} ([0-9]{4})$");
-		matcher = pattern.matcher(dateString);
-		if (matcher.find()) {
-			Integer value = Integer.parseInt(dateString.substring(7,11));
-			result = (value * 10000);
-
-			value = Integer.parseInt(dateString.substring(0,2));
-			result = result + value;
-
-			value = months.indexOf(dateString.substring(3, 6).toUpperCase());
-			result = result + ((value/3)+1)*100;
-		}
-		
-		pattern = Pattern.compile("^[a-zA-Z]{3} ([0-9]{4})");
-		matcher = pattern.matcher(dateString);
-		if (matcher.find()) {
-			Integer value = Integer.parseInt(dateString.substring(4,8));
-			result = (value * 10000);
+			if (dateString.substring(0, 3).equalsIgnoreCase("bef")) {
+				dateString = dateString.substring(4);
+				adjustment = -1;
+			}
+	
+			if (dateString.substring(0, 3).equalsIgnoreCase("aft")) {
+				dateString = dateString.substring(4);
+				adjustment = 1;
+			}
+	
+			Pattern pattern = Pattern.compile("^[0-9]{4}");
+			Matcher matcher = pattern.matcher(dateString);
+			if (matcher.find()) {
+				result = Integer.parseInt(dateString.substring(0,4));
+			}
+	
+			pattern = Pattern.compile("^Q[0-9]{1} ([0-9]{4})$");
+			matcher = pattern.matcher(dateString);
+			if (matcher.find()) {
+				Integer value = Integer.parseInt(dateString.substring(3,7));
+				result = (value * 10000) + 100;
+				
+				value = Integer.parseInt(dateString.substring(1, 2));
+				result = result + ((value - 1) * 300);
+			}
 			
-			value = months.indexOf(dateString.substring(0, 3).toUpperCase());
-			result = result + ((value/3)+1)*100;
+			pattern = Pattern.compile("^[0-9]{2} [a-zA-Z]{3} ([0-9]{4})$");
+			matcher = pattern.matcher(dateString);
+			if (matcher.find()) {
+				Integer value = Integer.parseInt(dateString.substring(7,11));
+				result = (value * 10000);
+	
+				value = Integer.parseInt(dateString.substring(0,2));
+				result = result + value;
+	
+				value = months.indexOf(dateString.substring(3, 6).toUpperCase());
+				result = result + ((value/3)+1)*100;
+			}
+			
+			pattern = Pattern.compile("^[a-zA-Z]{3} ([0-9]{4})");
+			matcher = pattern.matcher(dateString);
+			if (matcher.find()) {
+				Integer value = Integer.parseInt(dateString.substring(4,8));
+				result = (value * 10000);
+				
+				value = months.indexOf(dateString.substring(0, 3).toUpperCase());
+				result = result + ((value/3)+1)*100;
+			}
+	
+			result = result + adjustment;
 		}
-
-		result = result + adjustment;
 		
 		return result;
 	}
